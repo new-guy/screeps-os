ad79615fe03367d98fe8298de64d6e176c13f301
David Kirk <flagbearer223@gmail.com>
2019-08-21 13:43:16 -0500

    Update EnergyHarvestingManager.js

--- js/Mining/EnergyHarvestingManager.js
+++ js/Mining/EnergyHarvestingManager.js
@@ -1,7 +1,7 @@
 const Process = require('Process');
 
 var MAX_TICKS_TO_USE_PER_SPAWN = 700;
-var TARGET_ROUTES_PER_STORAGE = 6;
+var TARGET_ROUTES_PER_STORAGE = 5;
 
 var TIME_BETWEEN_PURGES = 1500;
 
