27ba2f8c07820d4247ff4fb1b277451addfb1522
David Kirk <david.kirk@thentwrk.com>
2022-08-18 22:28:08 -0500

    RR

---
+++ js/Creeps/RoadRepairer.js
@@ -0,0 +1,83 @@
+const CreepProcess = require('CreepProcess');
+
+class RoadRepairer extends CreepProcess {
+    constructor (...args) {
+        super(...args);
+
+        if(this.creep != null) {
+            this.targetColony = Game.colonies[this.creep.memory.targetColony];
+        }
+    }
+
+    updateStateTransitions() {
+        var state = this.creep.memory.state;
+        if(state == undefined) {
+            state = 'getEnergy';
+        }
+
+        if(state === 'getEnergy') {
+            if(this.creep.hasFullEnergy) {
+                state = 'work'
+                this.creep.clearTarget();
+            }
+        }
+
+        else if(state === 'work') {
+            if(this.creep.hasNoEnergy) {
+                state = 'getEnergy'
+                this.creep.clearTarget();
+            }
+        }
+
+        this.creep.memory.state = state;
+    }
+
+    performStateActions() {
+        var state = this.creep.memory.state;
+        if(state === 'getEnergy') {
+            this.creep.getEnergyFromClosestColonyHarvestDestination(this.spawningColony, true);
+        }
+
+        else if(state === 'work') {
+            this.work();
+        }
+    }
+
+    work() {
+        var target = this.creep.getTarget();
+
+        if(target == null) {
+            this.determineTarget();
+            target = this.creep.getTarget();
+        }
+
+        this.creep.repairTarget();
+    }
+
+    determineTarget() {
+        // - Go to room that needs critical repairs
+        // - Build/repair in current room
+        // - Build in room that most needs construction
+
+        var targetRoom = this.getTargetRoom();
+
+        if(targetRoom == null) {
+            this.creep.say('NoRoom');
+        }
+
+        else {
+            this.creep.say(targetRoom.name);
+            if(targetRoom.mostDamagedRoad != null) {
+                this.creep.setTarget(targetRoom.mostDamagedRoad);
+            }
+        }
+    }
+
+    getTargetRoom() {
+        if(this.targetColony.roomNeedingRoadRepairs != null) {
+            return this.targetColony.roomNeedingRoadRepairs;
+        }
+    }
+}
+
+module.exports = RoadRepairer;
